22ce0549b7731c02852e7b2595efc4e81667474a
bishtntl <bishtntl04@gmail.com>
2023-11-22 22:59:48 +0530

    okk

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express=require("express")
+const express = require("express");
 // const blogRouter=require("./route/blogroute")
-const productRoute=require("./route/procductRote")
-const app=express()
-app.use(express.json())
-const cors=require("cors")
+const searchroute = require("./route/searchrouter");
+const productRoute = require("./route/procductRote");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
 
-const  connection  = require("./config/db")
-const routeone = require("./route/userrouter")
-const port=process.env.PORT  || 5050
+const connection = require("./config/db");
+const routeone = require("./route/userrouter");
+const port = process.env.PORT || 5050;
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(cors({
-    origin:"*"
-}))
-
-
-app.use(express.urlencoded({extended:true
-}))
-app.use("/api",routeone)
-app.use("/api",productRoute)
+app.use(express.urlencoded({ extended: true }));
+app.use( searchroute);
+app.use("/api", routeone);
+app.use("/api", productRoute);
 // app.use("/api",blogRouter)
-app.listen(port,async()=>{
-    try{
-        await connection()
-        console.log("server is runnig")
-    }
-    catch(err){
-        console.log(err,"error")
-    }
-})
+app.listen(port, async () => {
+  try {
+    await connection();
+    console.log("server is runnig");
+  } catch (err) {
+    console.log(err, "error");
+  }
+});
--- model/productmodel.js
+++ model/productmodel.js
@@ -1,23 +1,16 @@
+const mongoose = require("mongoose");
 
-const mongoose=require("mongoose")
+const productSchema = mongoose.Schema({
+  name: String,
+  id: Number,
+  price: Number,
+  category: String,
+  heading: String,
+  image: String,
+  images: String,
+  imagess: String,
+  para: String,
+});
+const productColletion = mongoose.model("productColletion_All", productSchema);
 
-const productSchema=mongoose.Schema(
-    {
-        name:String,
-        id:Number,
-        price:Number,
-        category:String,
-        heading:String,
-        image:String,
-        images:String,
-        imagess:String,
-        para:String
-
-    }
-
-
-  
-)
-const productColletion=mongoose.model("productColletion_All",productSchema)
-
-module.exports= productColletion;
+module.exports = productColletion;
